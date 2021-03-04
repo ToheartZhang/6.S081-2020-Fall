19d747fcf549ae905370546ab140441f930b7d4d
root <root@openEuler.localdomain>
2021-03-04 20:01:08 +0800

    finish lab 4

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,6 +82,41 @@ struct trapframe {
 
 enum procstate { UNUSED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+struct user_context {
+  uint64 epc;
+  uint64 ra;
+  uint64 sp;
+  uint64 gp;
+  uint64 tp;
+  uint64 t0;
+  uint64 t1;
+  uint64 t2;
+  uint64 s0;
+  uint64 s1;
+  uint64 a0;
+  uint64 a1;
+  uint64 a2;
+  uint64 a3;
+  uint64 a4;
+  uint64 a5;
+  uint64 a6;
+  uint64 a7;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+  uint64 t3;
+  uint64 t4;
+  uint64 t5;
+  uint64 t6;
+};
+
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -103,4 +138,10 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  void (*handler)();
+  int alarm_interval;
+  int tick_times;
+  int is_in_handler;
+  struct user_context alarm_context;
 };
